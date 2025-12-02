cf61bc6a4c6eb8f7a8324c0f37ec95c02128cb01
Olha Kabatska <kabatskaya.olga@gmail.com>
2025-12-02 20:55:58 +0100

    feat: Enhance DashboardLayout with dynamic link highlighting and additional imports

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { ReactNode, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 import { useAuth } from "@/contexts/AuthContext";
 import { LayoutDashboard, LogOut } from "lucide-react";
 import Link from "next/link";
 import IconButton from "@/components/ui/IconButton";
+import { Parastoo } from "next/font/google";
 
 const parentsLinks = [
   {
@@ -27,6 +28,8 @@ const parentsLinks = [
 ];
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   const { user, loading, logout } = useAuth();
+  const pathname = usePathname();
+  console.log("DashboardLayout parent param:", parent, pathname);
   const router = useRouter();
 
   useEffect(() => {
@@ -83,7 +86,9 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
               <Link
                 key={link.href}
                 href={link.href}
-                className="flex items-center gap-3 px-3 py-2.5 text-slate-900 bg-slate-100 rounded-lg font-medium"
+                className={`flex items-center gap-3 px-3 py-2.5 text-slate-900  rounded-lg font-medium ${
+                  pathname === link.href ? "bg-slate-200" : "bg-slate-100"
+                }`}
               >
                 <link.icon className="w-5 h-5" />
                 <span>{link.label}</span>
